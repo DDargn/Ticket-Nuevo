00508cda3002f1271a4a16b2725262d6e0cbfa09
Ivan Fernandez Aroca <117987308+DDargn@users.noreply.github.com>
2024-04-18 08:08:02 +0200

    Modificacion en los ficheros php

--- Android/v1/getTaskAssigned.php
+++ Android/v1/getTaskAssigned.php
@@ -6,7 +6,7 @@ $password_localhost="";
 
 $json=array();
     $conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
-    $cosulta ="select fk_iduser, title, text from tasks where fk_iduser = {$_POST['fk_iduser']} and type = 1";
+    $cosulta ="select fk_iduser, title, text, Date from tasks where fk_iduser = {$_POST['fk_iduser']} and type = 1";
     $resultado=mysqli_query($conexion,$cosulta);
     while($registro=mysqli_fetch_assoc($resultado)){
         $json['Tasks'][]=$registro;
